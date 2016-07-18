be7a799064305f5569319f956c789890c70d7f77
Luke Denton <luke.denton@aligent.com.au>
2016-07-18 10:04:17 +0930

    Added new images task

---
+++ tasks/images.js
@@ -0,0 +1,35 @@
+/* jshint node: true */
+'use strict';
+
+var _ = require('lodash'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps');
+
+var imagesDefaults = {
+    name: 'images'
+};
+
+
+module.exports = function (gulp, options) {
+    var imagesOptions = {},
+        paths = options.paths;
+
+    // Set options
+    _.merge(imagesOptions, imagesDefaults, options.images);
+
+    gulp.task(imagesOptions.name, function () {
+        return gulp.src(options.paths.src.images, {base: './'})
+            .pipe(sourcemaps.init())
+            .pipe(imagemin())
+            .pipe(rename(function (path) {  // Replace the source paths with destination ones
+                path.dirname = path.dirname.replace(paths.dest.regex, paths.dest.replacement);
+            }))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('./'));
+    });
+
+    gulp.task(imagesOptions.name + ':watch', function () {
+        gulp.watch(paths.src.js, [imagesOptions.name]);     // TODO consider changing to gulp-watch so new files are detected
+    });
+};
